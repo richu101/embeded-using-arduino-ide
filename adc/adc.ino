a1786f12c3266c016686e050eda554ba86e26a45
richu101 <richubini101@gmail.com>
2020-11-21 17:31:39 +0000

    print adc val serialy adn arduino cli commands .md is added

--- adc/adc.ino
+++ adc/adc.ino
@@ -4,16 +4,17 @@ static inline void adc0init(void)
     ADCSRA |= (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0);
     ADCSRA |= (1<<ADEN);
 }
+  uint16_t adcval;
 void setup() 
 {
-    uint16_t adcval;
+  
     adc0init();
     Serial.begin(9600);
 }
 
 void loop() 
 {
-    ADCSRA |= (1<<ADCSC);
+    ADCSRA |= (1<<ADSC);
     loop_until_bit_is_clear(ADCSRA,ADSC);
     adcval = ADCH;
     Serial.println(adcval);
