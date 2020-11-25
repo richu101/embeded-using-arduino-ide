749aa0dbd22d49e251c3d51146ed6918b89a73f7
richu101 <richubini101@gmail.com>
2020-11-25 07:51:45 +0000

    INT0 intrrept added :snowman:

--- intrrepts/external_intrrept0/external_intrrept0.ino
+++ intrrepts/external_intrrept0/external_intrrept0.ino
@@ -1,18 +1,32 @@
 
 ISR(INT_vect)
 {
-    
+    if (bit_is_clear(PINE, PE4))
+    {
+        PORTB |= (1 << 6);
+    }
+    else
+    {
+        PORTB |= (1 << 6);
+    }
 }
 
-
-
-void setup() {
-
-    DDRB |= (1<<7) | (1<<6);
-    DDRE |= (1<<4);    
-
-
+void intrrept_init()
+{
+    EIMSK |= (1<<INT0);
+    EICRA |= (1<<ISC00);
+    sei();
 }
 
-void loop() {
+void setup()
+{
+
+    DDRB |= (1 << 7) | (1 << 6);
+    PORTE |= (1 << 4); //enabling the internal pull up
+}
+
+void loop()
+{
+    PORTB ^= (1<<7);
+    delay(500);
 }
