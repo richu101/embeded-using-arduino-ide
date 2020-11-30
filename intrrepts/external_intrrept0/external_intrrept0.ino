2e3800709df90cf29659d97755b79af9915306d8
richu101 <richubini101@gmail.com>
2020-11-30 17:46:32 +0000

    ok

--- intrrepts/external_intrrept0/external_intrrept0.ino
+++ intrrepts/external_intrrept0/external_intrrept0.ino
@@ -1,10 +1,10 @@
 ISR(INT4_vect)
 {
-    if (bit_is_set(PINE,4))
+    if (bit_is_set(PINE,4)) // When the pin change the state low to high then the led will turn on
     {
         PORTB |= (1 << 7);
         _delay_ms(500);
-        Serial.println("1");
+        Serial.println("am in the intrrept");
         PORTB &= (0 << 7);
         _delay_ms(500);
     }
@@ -17,7 +17,8 @@ ISR(INT4_vect)
 }
 
 void intrrept_init(void)
-{
+{ 
+    // This function is to initilize the intrrepts
     EIMSK |= (1<<INT4);
     EICRB |= (1<<ISC40);
     sei();
@@ -30,7 +31,9 @@ void setup()
     DDRB |= 0xff; // set DDRB pins as 
     DDRE |= (0<<2)  | (0<<3);
      //PORTE |= (1<<4) ; //enabling the internal pull up
+    //desabled the intrrepts
     intrrept_init();
+    
 }
 
 void loop()
@@ -38,7 +41,6 @@ void loop()
      delay(1000);
      PORTB ^= (1<<6);
     
-   
 }
 
 
